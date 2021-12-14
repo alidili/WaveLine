6cddbfc3cf2d768cf46aa1ccf3c338737ebdbfb4
yangle <mr.yang0229@foxmail.com>
2021-12-14 17:02:22 +0800

    增加示例gif、apk

--- app/src/main/java/com/waveline/exm/MainActivity.java
+++ app/src/main/java/com/waveline/exm/MainActivity.java
@@ -1,13 +1,8 @@
 package com.waveline.exm;
 
-import static android.Manifest.permission.RECORD_AUDIO;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 
-import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.yl.waveline.WaveLineView;
 
 public class MainActivity extends Activity {
@@ -20,14 +15,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         waveLineView = (WaveLineView) findViewById(R.id.waveLineView);
         waveLineView.startAnim();
-
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-            new RxPermissions(MainActivity.this)
-                    .requestEach(RECORD_AUDIO, WRITE_EXTERNAL_STORAGE)
-                    .subscribe(permission -> {
-
-                    });
-        }
     }
 
     @Override
